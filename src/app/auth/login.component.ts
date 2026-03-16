b5ee0fda1b09f8296bb2ee1122090bbc26948933
dmtr <12nelson13@gmail.com>
2026-03-16 19:56:31 +0200

    chore: upgrade to Angular 14

--- src/app/auth/login.component.ts
+++ src/app/auth/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UntypedFormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
 import { finalize } from 'rxjs/operators';
 
 import { environment } from '@env/environment';
@@ -18,13 +18,13 @@ const log = new Logger('Login');
 export class LoginComponent implements OnInit {
   version: string | null = environment.version;
   error: string | undefined;
-  loginForm!: FormGroup;
+  loginForm!: UntypedFormGroup;
   isLoading = false;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private formBuilder: FormBuilder,
+    private formBuilder: UntypedFormBuilder,
     private authenticationService: AuthenticationService
   ) {
     this.createForm();
