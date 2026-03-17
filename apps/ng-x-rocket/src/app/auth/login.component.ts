cc2c6621cddbbb34407550acbea577648ecd6cb1
dmtr <12nelson13@gmail.com>
2026-03-17 19:41:37 +0200

    chore: migrate to Nx workspace, create libs structure

---
+++ apps/ng-x-rocket/src/app/auth/login.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UntypedFormGroup, UntypedFormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { finalize } from 'rxjs/operators';
+
+import { environment } from '@env/environment';
+import { Logger, UntilDestroy, untilDestroyed } from '@core';
+import { AuthenticationService } from './authentication.service';
+import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
+import { LanguageSelectorComponent } from '../i18n/language-selector.component';
+
+const log = new Logger('Login');
+
+@UntilDestroy()
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  imports: [TranslateDirective, LanguageSelectorComponent, ReactiveFormsModule, TranslatePipe],
+})
+export class LoginComponent implements OnInit {
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+  private formBuilder = inject(UntypedFormBuilder);
+  private authenticationService = inject(AuthenticationService);
+
+  version: string | null = environment.version;
+  error: string | undefined;
+  loginForm!: UntypedFormGroup;
+  isLoading = false;
+
+  constructor() {
+    this.createForm();
+  }
+
+  ngOnInit() {}
+
+  login() {
+    this.isLoading = true;
+    const login$ = this.authenticationService.login(this.loginForm.value);
+    login$
+      .pipe(
+        finalize(() => {
+          this.loginForm.markAsPristine();
+          this.isLoading = false;
+        }),
+        untilDestroyed(this),
+      )
+      .subscribe(
+        (credentials) => {
+          log.debug(`${credentials.username} successfully logged in`);
+          this.router.navigate([this.route.snapshot.queryParams.redirect || '/'], { replaceUrl: true });
+        },
+        (error) => {
+          log.debug(`Login error: ${error}`);
+          this.error = error;
+        },
+      );
+  }
+
+  private createForm() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      remember: true,
+    });
+  }
+}
---
+++ apps/ng-x-rocket/src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+
+import { QuoteService } from './quote.service';
+import { TranslateDirective } from '@ngx-translate/core';
+import { LoaderComponent } from '../@shared/loader/loader.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  imports: [TranslateDirective, LoaderComponent],
+})
+export class HomeComponent implements OnInit {
+  private quoteService = inject(QuoteService);
+
+  quote: string | undefined;
+  isLoading = false;
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.quoteService
+      .getRandomQuote({ category: 'dev' })
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+        }),
+      )
+      .subscribe((quote: string) => {
+        this.quote = quote;
+      });
+  }
+}
